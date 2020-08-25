f8acfc690c071dc7ec729359c027e086a335a2e9
Sinwan <sinwan@kefassoftwares.com>
2020-08-25 12:58:10 +0530

    new form

--- js/main.js
+++ js/main.js
@@ -199,23 +199,6 @@ jQuery(document).ready(function($) {
 	siteMenuClone();
 
 
-	var sitePlusMinus = function() {
-		$('.js-btn-minus').on('click', function(e){
-			e.preventDefault();
-			if ( $(this).closest('.input-group').find('.form-control').val() != 0  ) {
-				$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
-			} else {
-				$(this).closest('.input-group').find('.form-control').val(parseInt(0));
-			}
-		});
-		$('.js-btn-plus').on('click', function(e){
-			e.preventDefault();
-			$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
-		});
-	};
-	sitePlusMinus();
-
-
 	var siteSliderRange = function() {
     $( "#slider-range" ).slider({
       range: true,
@@ -264,23 +247,6 @@ jQuery(document).ready(function($) {
 	  });
 	};
 	siteMagnificPopup();
-
-	var searchShow = function() {
-		// alert();
-		var searchWrap = $('.search-wrap');
-		$('.js-search-open').on('click', function(e) {
-			e.preventDefault();
-			searchWrap.addClass('active');
-			setTimeout(function() {
-				searchWrap.find('.form-control').focus();
-			}, 300);
-		});
-		$('.js-search-close').on('click', function(e) {
-			e.preventDefault();
-			searchWrap.removeClass('active');
-		})
-	};
-	searchShow();
 
 	$('.nav-item').on('click',function(){
 
