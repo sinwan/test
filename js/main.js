ae70ddb76d94a1301762615447faa80f770b9c27
Sinwan <sinwan@kefassoftwares.com>
2020-08-26 12:32:34 +0530

    cl

--- js/main.js
+++ js/main.js
@@ -299,7 +299,7 @@ jQuery(document).ready(function($) {
 
 		var mailto_link = 'mailto:' + email + '?subject=' +
 											subject + '&body= Hi, I am '+name +
-											','+ body_message;
+											',<br>'+ body_message;
     win = window.open(mailto_link, 'emailWindow');
     if (win && win.open && !win.closed) win.close();
 	});
